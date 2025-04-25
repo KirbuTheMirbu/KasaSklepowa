010c27e56235135951355e6f5ac9a40f26c809d4
KirbuTheMirbu <166061097+KirbuTheMirbu@users.noreply.github.com>
2025-04-25 13:18:00 +0200

    25.04.2025

--- Bank/src/main/java/org/example/bank/Objects/PlatnoscBlik.java
+++ Bank/src/main/java/org/example/bank/Objects/PlatnoscBlik.java
@@ -1,29 +1,20 @@
 package org.example.bank.Objects;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.Console;
-import java.sql.Connection;
-import java.sql.DriverManager;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.aop.scope.ScopedProxyUtils;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
 
-@RestController
-@RequestMapping("/api")
-public class BlikController {
-
+public class PlatnoscBlik {
     private static final Logger log = LoggerFactory.getLogger(BlikController.class);
 
-    @PostMapping("/blik")
+    @PostMapping("/blikPl")
     public String dodajKod(@RequestBody Blik blik/*, UriComponentsBuilder uriComponentsBuilder*/) throws SQLException, InterruptedException {
         String url = "jdbc:mariadb://localhost:3306/bank";
         String user = "root";
